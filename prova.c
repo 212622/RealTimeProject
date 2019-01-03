56988cd306c8a5d18b05c71f8f4c3f634c81c80b
212622 <212622@studenti.unimore.it>
2019-01-03 12:54:29 +0100

    traiettorie casuali

--- prova.c
+++ prova.c
@@ -21,23 +21,33 @@ int main(int argc, char const *argv[])
 	ALLEGRO_BITMAP *razzo = bitmap("img/rocket.png");
 	ALLEGRO_BITMAP *boom = bitmap("img/Pow.bmp");
 
-	float x = rand()%1024, y = 0;
+	float x = rand()%1024, y = 0, alfa = rand()%3;
+	int a;
 	speed = 2 + rand()%20;
 	while(1){
 		al_clear_to_color(BLACK);
 		al_draw_bitmap(sfondo, 0, 600, 0);
+		//alfa usata per decidere se il razzo si sposta a destra sinistra o centrale
+		if(alfa == 0)
+			a = 1;
+		if(alfa == 1)
+			a = -1;
+		if(alfa == 2)
+			a = 0;
 		//Se premo ESC esco
 		al_get_keyboard_state(&state);
 		if(al_key_down(&state, ALLEGRO_KEY_ESCAPE)){
 			break;
 		}
 		//se il razzo non è arrivato alla citta scende, altrimenti scoppia
-		if(y < 600-al_get_bitmap_height(razzo)){
+		if(x < 1024 && x > 0 && y < 600-al_get_bitmap_height(razzo)){
 			y = y + speed;
+			x = x - (a * speed/5);
 			al_draw_bitmap(razzo, x, y, 0);
 		}else{
 			al_draw_bitmap(boom, x+50-al_get_bitmap_width(boom), y, 0);
 			x = rand()%1024;
+			alfa = rand()%3;
 			y = 0;
 			speed = 2 + rand()%20;
 		}
