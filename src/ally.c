15aff0f21277f3a5666a0cc5bb5688a60ae677f3
212622 <212622@studenti.unimore.it>
2019-01-29 11:38:57 +0100

    ally update

--- src/ally.c
+++ src/ally.c
@@ -23,13 +23,23 @@ int     al_deadline;
 
 pthread_mutex_t mal;						// ally mutex
 /*----------------------------------------------------------------------*/
+/*  FUNCTION DEFINITIONS   */
+/*----------------------------------------------------------------------*/
+//	WRITE_AL_ATT: write allies attributes in global variables.
+/*----------------------------------------------------------------------*/
+void write_al_att(float x1, float y1, float m, int tid) {
+	pthread_mutex_lock(&mal);
+	ally_x[tid] = x1;
+	ally_y[tid] = y1;
+	al_angle[tid] = m;
+	pthread_mutex_unlock(&mal);
+}
+/*----------------------------------------------------------------------*/
 //	ALLY: periodic task for ally
 /*----------------------------------------------------------------------*/
 void ally(void) {
-
-	int tid = ptask_get_index() - (MAXE + 1);
-	float x1, x2, y1, y2, speed = 8;
-	float m;
+	int tid = ptask_get_index() - (MAXE + 1);	// task array index
+	float m, x1, x2, y1, y2, speed = 8;			// ally position and speed variables
 
 	x1 = (XWORLD / 2) - (razzo->w / 2) + (BORDER * 3);
 	y1 = YWORLD - sfondo->h - razzo->h;
@@ -41,12 +51,7 @@ void ally(void) {
 	speed =  ((y1 - y2) / ((y2 - line_y1) / en_speed)) * CONVERSION;
 	while(1) {
 		if (y1 > HRES + 1 && crash_al[tid] == 0) {
-			pthread_mutex_lock(&mal);
-			ally_x[tid] = x1;
-			ally_y[tid] = y1;
-			al_angle[tid] = m;
-			pthread_mutex_unlock(&mal);
-			
+			write_al_att(x1, y1, m , tid);
 			x1 += (-speed / (y2 - y1)) * (x2 - x1);
 			y1 -= speed;
 		}
@@ -65,16 +70,11 @@ void ally(void) {
 			y2 = YWORLD / 2;
 			x2 = (((y2 - line_y1) / (line_y2 - line_y1)) * (line_x2 - line_x1)) + line_x1;
 			m = (y2 - y1) / (x2 - x1);
-			pthread_mutex_lock(&mal);
-			ally_x[tid] = x1;
-			ally_y[tid] = y1;
-			al_angle[tid] = m;
-			pthread_mutex_unlock(&mal);
+			write_al_att(x1, y1, m, tid);
 		}
 
 		/* check for deadline miss */
         if (ptask_deadline_miss()) al_deadline++;
-
 		ptask_wait_for_period();
 	}
 }
